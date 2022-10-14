b94b14325a3b8b9e08419d9d018668dd68fe8885
jodimcjodson <83870316+DaQueenJodi@users.noreply.github.com>
2022-10-14 17:47:29 -0500

    done uwu

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 {pkgs ? import <nixpkgs> {}}:
 pkgs.mkShell {
-  nativeBuildInputs = with pkgs; [rustfmt rust-analyzer rustc cargo xorg.libX11 xorg.libXcursor xorg.libxcb pkg-config];
+  nativeBuildInputs = with pkgs; [github-cli rustfmt rust-analyzer rustc cargo xorg.libX11 xorg.libXcursor xorg.libxcb pkg-config];
   RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
 }
